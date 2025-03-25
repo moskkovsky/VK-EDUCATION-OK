ddeef148297eba15f5544d8c56e613b0cd325f0d
aalmoskovskiy <aalmoskovskiy@sberbank.ru>
2025-03-25 17:26:49 +0300

    test[Авторизация]: Тесты + Page

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,69 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import config.EnvConfig;
+import io.qameta.allure.Step;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * Страница для входа в OK
+ */
+public class LoginPage {
+    private static final Logger log = LogManager.getLogger(LoginPage.class);
+    private final SelenideElement fieldInputEmail = $x("//input[@id='field_email']");
+    private final SelenideElement fieldInputPassword = $(byId("field_password"));
+    private final SelenideElement buttonSignInOK = $x("//input[@value='Войти в Одноклассники']");
+    private static final String ERROR_MESSAGE_TEMPLATE = "//div[text()='%s']"; // Шаблон для ошибок на страницы Авторизации
+
+    @Step("Проходим авторизацию с почтой пользователя")
+    public LoginPage auth() {
+        log.info("Авторизация с логином и паролем");
+        inputUserEmail();
+        inputUserPassword();
+        clickButtonSignOk();
+        return this;
+    }
+
+    @Step("Проверяем ошибку '{errorMessageValue}'")
+    public LoginPage checkErrorMessage(String errorMessageValue) {
+        log.info(String.format("Проверяем ошибку на странице Авторизации: %s", errorMessageValue));
+        SelenideElement errorMessageOnLoginPage = $x(String.format(ERROR_MESSAGE_TEMPLATE, errorMessageValue));
+        errorMessageOnLoginPage
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.exactText(errorMessageValue));
+        return this;
+    }
+
+    @Step("Вводим логин пользователя")
+    public LoginPage inputUserEmail() {
+        log.info("Вводим логин пользователя");
+        fieldInputEmail.shouldBe(Condition.visible).clear();
+        fieldInputEmail.sendKeys(EnvConfig.USER_LOGIN);
+        return this;
+    }
+
+    @Step("Вводим пароль пользователя")
+    public LoginPage inputUserPassword() {
+        log.info("Вводим пароль пользователя");
+        fieldInputPassword.shouldBe(Condition.visible).clear();
+        fieldInputPassword.sendKeys(EnvConfig.USER_PASSWORD);
+        return this;
+    }
+
+    @Step("Нажимаем на кнопку Войти в Одноклассники")
+    public LoginPage clickButtonSignOk() {
+        log.info("Нажимаем на кнопку Войти на странице Авторизации");
+        buttonSignInOK.shouldBe(Condition.visible).click();
+        return this;
+    }
+
+    public Navigation getNavigation() {
+        return new Navigation();
+    }
+}
---
+++ src/test/java/tests/CheckLoginTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+
+import core.SelenideDriver;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+import pages.TapePage;
+
+import static constants.tape.TapeValues.BUTTON_HOBBIES;
+
+@Epic(value = "Авторизация")
+@DisplayName("Тесты для Авторизации пользователя")
+@Link("https://t.me/moskkovsky")
+public class CheckLoginTest extends SelenideDriver {
+    private LoginPage loginPage = new LoginPage();
+    private TapePage tapePage = new TapePage();
+
+    @Test
+    @Feature(value = "Вход по логину и паролю")
+    @Story(value = "Успешная авторизация с валидными данными")
+    @DisplayName("Прохождение авторизации")
+    @Description("Тест проверяет успешное прохождение авторизации и с проверкой, что после нажатия на кнопку Войти в Одноклассники переход на страницу Лента")
+    @Owner("Anton Moskovsky")
+    @Severity(value = SeverityLevel.BLOCKER)
+    public void testCheckSuccessAuth() {
+        loginPage.auth();
+        tapePage.checkButtonVisibleOnTapePage(BUTTON_HOBBIES);
+    }
+
+    /**
+     * Верну тесты, когда скинут тестового пользователя
+     */
+    /*
+    @Test
+    @Feature(value = "Получение ошибки в случае пустого ввода")
+    @DisplayName("Получение ошибки при пустом логине")
+    @Description("Тест проверяет на получение ошибки, в случае если вводим пустой логин")
+    @Owner("Anton Moskovsky")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void testCheckVisibleErrorMessageWithEmptyLogin() {
+        loginPage.inputUserEmail("")
+                .inputUserPassword(System.getenv("USER_PASSWORD"))
+                .clickButtonSignOk()
+                .checkErrorMessage(LoginErrorMessageValues.ERROR_MESSAGE_EMPTY_LOGIN);
+    }
+
+    @Test
+    @Feature(value = "Получение ошибки в случае пустого ввода")
+    @DisplayName("Получение ошибки при пустом пароле")
+    @Description("Тест проверяет на получение ошибки, в случае если вводим пустой пароль")
+    @Owner("Anton Moskovsky")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void testCheckVisibleErrorMessageWithEmptyPassword() {
+        loginPage.inputUserEmail()
+                .inputUserPassword("")
+                .clickButtonSignOk()
+                .checkErrorMessage(LoginErrorMessageValues.ERROR_MESSAGE_EMPTY_PASSWORD);
+    }
+
+    @Test
+    @Feature(value = "Получение ошибки в случае пустого ввода")
+    @DisplayName("Получение ошибки при пустом логине и пароле")
+    @Description("Тест проверяет на получение ошибки, в случае если вводим пустой логин и пароль")
+    @Owner("Anton Moskovsky")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void testCheckVisibleErrorMessageWithEmptyLoginAndPassword() {
+        loginPage.inputUserEmail("")
+                .inputUserPassword("")
+                .clickButtonSignOk()
+                .checkErrorMessage(LoginErrorMessageValues.ERROR_MESSAGE_EMPTY_LOGIN);
+    }
+
+    @Test
+    @Feature(value = "Получение ошибки в случае ввода неправильных данных")
+    @DisplayName("Получение ошибки при неправильном логине и пароле")
+    @Description("Тест проверяет на получение ошибки, в случае если вводим неправильный логин и пароль")
+    @Owner("Anton Moskovsky")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void testCheckVisibleErrorMessageWrongLogin() {
+        loginPage.inputUserEmail(ConfigProvider.WRONG_LOGIN)
+                .inputUserPassword(ConfigProvider.WRONG_PASSWORD)
+                .clickButtonSignOk()
+                .checkErrorMessage(LoginErrorMessageValues.ERROR_MESSAGE_WRONG_LOGIN_OR_PASSWORD);
+    }
+     */
+}
