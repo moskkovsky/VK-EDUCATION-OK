3a357f0ff53b3e26d9a3e64e50110ff84646ce82
aalmoskovskiy <aalmoskovskiy@sberbank.ru>
2025-04-25 17:17:49 +0300

    Добавил еще константы

---
+++ src/test/java/constants/valueInMethods/ValueMethods.java
@@ -0,0 +1,6 @@
+package constants.valueInMethods;
+
+public class ValueMethods {
+    static final public String CALL_METHOD_IS_LOADED = "Вызов метода isLoaded()";
+    static final public String CALL_METHOD_LOAD = "Вызов метода load()";
+}
